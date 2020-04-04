95eeaa8e7b3bc592d2454d2244dcaa6c9051932f
niklinkov <niklinkov71@gmail.com>
2020-04-04 18:49:44 +0300

    done 7 lesson home work

---
+++ src/Lesson_8/Exam.java
@@ -0,0 +1,150 @@
+package Lesson_8;
+
+public class Exam {
+
+
+//    public static void main(String[] args) {
+//
+//        big_loop:
+//
+//        for (int i = 0; i < 3 ; i++) {
+//
+//            try {
+//
+//                for (int j = 0; j < 3 ; j++) {
+//
+//                    if (i == j) continue;
+//
+//                    else if (i > j) continue big_loop;
+//
+//                    System.out.print("A");
+//
+//                }
+//
+//            } finally {
+//
+//                System.out.print("B");
+//
+//            }
+//
+//            System.out.print("C");
+//
+//        }
+//
+//    }
+
+//    public static void main(String[] args) {
+//
+//        Integer i1 = 10;
+//
+//        Integer i2 = 10;
+//
+//        Double d1 = 10d;
+//
+//        Double d2 = 10d;
+//
+//
+//
+//        System.out.println(i1 == i2);
+//
+//        System.out.println(d1 == d2);
+//
+//
+//
+//    }
+//    static void method(int... a) {
+//
+//    System.out.println("inside int...");
+//
+//}
+//
+//    static void method(long a, long b) {
+//
+//        System.out.println("inside long");
+//
+//    }
+//
+//    static void method(Integer a, Integer b) {
+//
+//        System.out.println("inside INTEGER");
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int a = 2;
+//
+//        int b = 3;
+//
+//        method(a,b);
+//
+//    }
+
+
+//    public static void main(String[] args) {
+//
+//        int a = 2;
+//
+//        int b = 10;
+//
+//        int c = 1 >> a++ + ++b - --a - b << 1;
+//
+//        System.out.println(a);
+//
+//        System.out.println(b);
+//
+//        System.out.println(c);
+//
+//    }
+
+//    public static void main(String[] args) {
+//
+//        String str1 = "Hello";
+//
+//        String str2 = "Hello";
+//
+//        System.out.println(str1 == str2);
+//
+//
+//
+//
+//
+//        String str3 = "Hell";
+//
+//        String str4 = str3 + "o";
+//
+//        String str5 = "Hello";
+//
+//
+//
+//        System.out.println(str4 == str5);
+//
+//    }
+
+//    public static void main(String[] args) {
+//
+//        String str = null;
+//
+//        int x = 10;
+//
+//        if(x < 0 & str.equals("hello")) {
+//
+//            System.out.println("java");
+//
+//        }
+//
+//
+//
+//    }
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i+=2) {
+
+            System.out.println(i++);
+
+        }
+
+    }
+
+}
--- src/Lesson_8/StringComparator.java
+++ src/Lesson_8/StringComparator.java
@@ -1,13 +1,28 @@
 package Lesson_8;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class StringComparator {
+
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        ArrayList<String> list = new ArrayList<>();
+        //получить первую строку
+        list.add(getString());
+        list.add(getString());
+
+
+        System.out.println(list.get(0).equalsIgnoreCase(list.get(1)));
+
     }
 
-    public getString (){
-
+    public static String getString (){
+        System.out.println("Vvedirte stroku");
+        return sc.nextLine();
     }
 
 }
